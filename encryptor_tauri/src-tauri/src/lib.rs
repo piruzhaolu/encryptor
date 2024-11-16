b89fdf992c55533bd44c6efc7ff69c3d108bdb0c
li pi <417347851@qq.com>
2024-11-16 19:26:45 +0800

    encryptor tauri

---
+++ encryptor_tauri/src-tauri/src/lib.rs
@@ -0,0 +1,129 @@
+use std::cmp::min;
+use std::fs;
+use std::path::Path;
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use base64::Engine;
+use aes_gcm::aead::{Aead, OsRng};
+use aes_gcm::{AeadCore, Aes256Gcm, Key, KeyInit, Nonce};
+use toml::Table;
+
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    let x = encrypt(name,name);
+    format!("Hello, {}! You've been greeted from Rust!", x)
+}
+
+
+
+#[tauri::command]
+fn get_keys(text: &str) -> String {
+
+    let value = text.parse::<Table>();
+    if let Ok(value) = value {
+        let mut list = vec![];
+        for k in value {
+            list.push(k.0);
+        }
+        list.join(",")
+    } else {  
+        "".to_string()
+    } 
+}
+
+#[tauri::command]
+fn find(toml:&str, text: &str, pw: &str) -> String {
+   
+    let value = toml.parse::<Table>().unwrap();
+    for k in value {
+        if k.0.contains(text) {
+            println!("# {}", k.0);
+            if let toml::Value::Table(table_value) = k.1 {
+                let p = &table_value["p"];
+                if let  toml::Value::String(p) = p {
+                    let pp: Vec<&str> = p.split("|").collect();
+                    let de = decrypt(pw,pp[0],pp[1]);
+                   // println!("---  {}", de);
+                    return  de;
+                }
+
+                for kv in table_value {
+                    println!("{} : {}", kv.0, kv.1);
+                }
+            }
+            // println!("site:{}; username:{}", k.0,k.1["n"]);
+        }
+    }
+    "".to_string()
+}
+
+
+
+
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(tauri::generate_handler![greet,get_keys,find])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+
+
+/// key 密码
+/// text 要加密的文本
+fn encrypt(key: &str, text: &str) -> String {
+    let key: &[u8; 32] = &u8_to_u832(key);
+    let key: &Key<Aes256Gcm> = key.into();
+
+    let key = Key::<Aes256Gcm>::from_slice(key);
+
+    let cipher = Aes256Gcm::new(&key);
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
+    let ciphertext = cipher.encrypt(&nonce, text.as_bytes().as_ref()).unwrap();
+    let nonce_string = base64::engine::general_purpose::STANDARD.encode(nonce.to_vec());
+    let base64_string = base64::engine::general_purpose::STANDARD.encode(&ciphertext);
+
+    format!("{}|{}", nonce_string, base64_string)
+    // return (base64_string,nonce_string);
+}
+
+
+fn decrypt(key: &str, nonce_base64_text: &str, base64_text: &str) -> String {
+    let key: &[u8; 32] = &u8_to_u832(key);
+    let key: &Key<Aes256Gcm> = key.into();
+
+    let key = Key::<Aes256Gcm>::from_slice(key);
+    let cipher = Aes256Gcm::new(&key);
+    // let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
+
+    let r = base64::engine::general_purpose::STANDARD.decode(base64_text);
+    let nonce = base64::engine::general_purpose::STANDARD.decode(nonce_base64_text).unwrap();
+    if let Ok(r) = r {
+        let ciphertext = cipher.decrypt(Nonce::from_slice(&nonce), r.as_ref());
+        if let Ok(ciphertext) = ciphertext {
+            let x = String::from_utf8(ciphertext);
+            if let Ok(x) = x {
+                return x;
+            }
+        }
+    }
+    "".to_string()
+}
+
+
+
+fn u8_to_u832(str: &str) -> [u8; 32] {
+    let bytes = str.as_bytes();
+    let len = min(bytes.len(), 32usize);
+    let mut u832: [u8; 32] = [0; 32];
+    for i in 0..len {
+        u832[i] = bytes[i];
+    }
+    u832
+}
---
+++ encryptor_tauri/src/App.tsx
@@ -0,0 +1,167 @@
+import {useEffect, useRef, useState} from "react";
+import {invoke} from "@tauri-apps/api/core";
+import "./App.css";
+import {BaseDirectory, readTextFile, writeTextFile} from "@tauri-apps/plugin-fs";
+import { writeText } from '@tauri-apps/plugin-clipboard-manager'; //readText
+import { message ,confirm} from '@tauri-apps/plugin-dialog';
+import CloseIcon from '@mui/icons-material/Close';
+import {
+    Autocomplete, Box,
+    Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, styled,
+    TextField
+} from "@mui/material";
+
+function App() {
+    const [toml, setToml] = useState("");
+    const [pwkeys, setPwkeys] = useState([""]);
+    const [platform, setPlatform] = useState<string | null>(null);
+    const [pw, setPw] = useState<string>("");
+    const [open, setOpen] = useState(false);
+    const editableRef = useRef();
+    // async function greet() {
+    //     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    //     setGreetMsg(await invoke("greet", {name}));
+    // }
+
+
+    async function writeTomlFile(fileContent: string) {
+        await writeTextFile("pw.toml", fileContent, {baseDir: BaseDirectory.AppLocalData});
+        // let xx = await readTextFile("pw.toml", {baseDir: BaseDirectory.AppLocalData});
+        // console.log(xx);
+    }
+
+    async function readTomlFile() {
+        let tomlText = await readTextFile("pw.toml", {baseDir: BaseDirectory.AppLocalData});
+        setToml(tomlText);
+        let keys: string = await invoke("get_keys", {text: tomlText});
+        let keysArray = keys.split(",");
+        setPwkeys(keysArray);
+        console.log(pwkeys);
+    }
+
+    //readTomlFile().then();
+    useEffect(() => {
+        if (toml === "") {
+            readTomlFile().then();
+        }
+    });
+
+    async function decode() {
+        if (platform === null || platform == "") {
+            handleClickOpen();
+            return;
+        }
+        let result: string = await invoke("find", {toml: toml, text: platform, pw: pw});
+     
+        if (result !== "") {
+            await writeText(result);
+            await message("密码已复制到剪贴板", {
+                title: 'Password',
+                kind: 'warning',
+            });
+        }
+    }
+
+    const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+        '& .MuiDialogContent-root': {
+            padding: theme.spacing(2),
+        },
+        '& .MuiDialogActions-root': {
+            padding: theme.spacing(1),
+        },
+    }));
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+    
+
+
+    async function handleSave() {
+        let htmlDiv:HTMLElement = editableRef.current as unknown as HTMLElement;
+        let newToml = htmlDiv?.innerText;
+        
+        if (newToml !== null && newToml !== undefined) {
+            if (toml != "") {
+                const answer = await confirm('覆盖当前Toml文件?', {
+                    title: 'Toml',
+                    kind: 'warning',
+                });
+                if (answer) {
+                    await writeTomlFile(newToml);
+                    await readTomlFile();
+                }
+            } else {
+                await writeTomlFile(newToml);
+                await readTomlFile();
+            }
+        }
+        setOpen(false);
+    }
+    
+
+
+    return (
+        <Box
+            display="flex"
+            flexDirection="column"
+            gap={2}
+            alignItems="left"
+            height="90vh" width={400}>
+
+            <Autocomplete
+                disablePortal
+                options={pwkeys || null}
+                sx={{width: 400}}
+                value={platform || null}
+                onChange={(_, newValue) => {
+                    setPlatform(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} label="平台"/>}
+            />
+            <TextField
+                id="outlined-password-input"
+                label="Password"
+                type="password"
+                value={pw || ""}
+                onChange={(e) => setPw(e.currentTarget.value)}
+            />
+            <Button variant="contained" onClick={decode}>Decode</Button>
+
+            <BootstrapDialog
+                onClose={handleClose}
+                aria-labelledby="customized-dialog-title"
+                open={open}
+            >
+                <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+                    Toml
+                </DialogTitle>
+                <IconButton
+                    aria-label="close"
+                    onClick={handleClose}
+                    sx={(theme) => ({
+                        position: 'absolute',
+                        right: 8,
+                        top: 8,
+                        color: theme.palette.grey[500],
+                    })}
+                >
+                    <CloseIcon />
+                </IconButton>
+                <DialogContent dividers contentEditable={true} sx={{width:'400px',height:'200px', outline:'none'}} ref={editableRef}>
+                    
+                </DialogContent>
+                <DialogActions>
+                    <Button autoFocus onClick={handleSave}>
+                        Save changes
+                    </Button>
+                </DialogActions>
+            </BootstrapDialog>
+        </Box>
+    );
+}
+
+export default App;
